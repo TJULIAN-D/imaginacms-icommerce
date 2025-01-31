eb47207d71705f31615b8c200e1adb876557476a
Cristian Marin <54483189+CristianMarin19@users.noreply.github.com>
2025-01-31 17:56:33 -0500

    Dynamic images from product Options
    * + fix | v10 22-01-25 product options and producto options value with trait mediaRelation and add parameter option in this entities
    
    * + fix | v10 22-01-25 add translation
    
    * + fix | v10 22-01-25 add new parameter in livewire event set option
    
    * + fix | v10 23-01-25 validate dynamic gallery only in option type color-image
    
    * + fix | v10 28-01-25 new utility dynamic single image
    
    * + fix | v10 30-01-25 add functionality in product list item and upgrade options with validation with param "Only Type"
    
    * + fix | v10 31-01-25 change parameter product to item in components media

--- Resources/views/frontend/components/product/product-list-item/layouts/product-list-item-layout-1/index.blade.php
+++ Resources/views/frontend/components/product/product-list-item/layouts/product-list-item-layout-1/index.blade.php
@@ -3,24 +3,22 @@
 @if(isset($discount) && $discount) with-discount @else without-discount @endif
 @if($product->is_sold_out) sold-out @endif ribbon-discount-position-{{$discountPosition}}
 @if($product->is_new) is-new @endif">
-    <x-isite::edit-link
-            link="{{$editLink}}{{$product->id}}"
-            tooltip="{{$tooltipEditLink}}"
-    />
+  <x-isite::edit-link
+    link="{{$editLink}}{{$product->id}}"
+    tooltip="{{$tooltipEditLink}}"
+  />
   @include('icommerce::frontend.components.product.meta')
-
   @if(isset($itemListLayout) && $itemListLayout=='one')
     <div class="row product-list-layout-one">
-
       <div class="col-6">
         <div class="row justify-content-center position-relative m-0">
           @include('icommerce::frontend.components.product.ribbon')
           <div
             class="bg-img bg-img-{{$imageAspect}} d-flex justify-content-center align-items-center overflow-hidden">
-            <x-media::single-image
+            <livewire:media::dynamic-image
               :alt="$product->name" :title="$product->name" :url="$product->url" :isMedia="true"
               :mediaFiles="$product->mediaFiles()"
-              imgClasses="product-img"/>
+              imgClasses="product-img" itemId="{{$product->id}}"/>
           </div>
         </div>
       </div>
@@ -29,13 +27,13 @@
       </div>
     </div>
   @else
-
-    <div class="bg-img bg-img-{{$imageAspect}} d-flex justify-content-center align-items-center overflow-hidden position-relative">
+    <div
+      class="bg-img bg-img-{{$imageAspect}} d-flex justify-content-center align-items-center overflow-hidden position-relative">
       @include('icommerce::frontend.components.product.ribbon')
-      <x-media::single-image
+      <livewire:media::dynamic-image
         :alt="$product->name" :title="$product->name" :url="$product->url" :isMedia="true"
         :mediaFiles="$product->mediaFiles()"
-        imgClasses="product-img" />
+        imgClasses="product-img" itemId="{{$product->id}}"/>
 
       @if(Str::contains($buttonsPosition, 'in-photo'))
         @include("icommerce::frontend.components.product.buttons")
@@ -43,15 +41,10 @@
     </div>
     @include('icommerce::frontend.components.product.product-list-item.layouts.product-list-item-layout-1.infor')
   @endif
-
   @include('icommerce::frontend.components.product.global-inline-css')
-
-  
- <!-- Validacion aca porque se reutiliza el mismo componente, con diferentes layouts en varias partes -->
-@if(setting('wishlistable::wishlistActive',null,false) && wishlistableShowBtn($currentRoute))
-  <!-- Wishlist Modal List | BTN -->
-  @include('wishlistable::frontend.partials.buttons',["type"=>"btn","entityName" => "Modules\\Icommerce\\Entities\\Product","entityId" => $product->id])
-@endif
-
- 
+  <!-- Validacion aca porque se reutiliza el mismo componente, con diferentes layouts en varias partes -->
+  @if(setting('wishlistable::wishlistActive',null,false) && wishlistableShowBtn($currentRoute))
+    <!-- Wishlist Modal List | BTN -->
+    @include('wishlistable::frontend.partials.buttons',["type"=>"btn","entityName" => "Modules\\Icommerce\\Entities\\Product","entityId" => $product->id])
+  @endif
 </div>
